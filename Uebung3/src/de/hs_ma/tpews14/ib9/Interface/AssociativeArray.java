0da7fb57f2b6ad81a6896c659d996f38551f9621
BrainSikk <christophhanschke@googlemail.com>
2014-11-13 16:30:04 +0100

    hab mal teil (b) versucht. Aber net alles hinbekommen auf die schnelle^^

--- Uebung3/src/de/hs_ma/tpews14/ib9/Interface/AssociativeArray.java
+++ Uebung3/src/de/hs_ma/tpews14/ib9/Interface/AssociativeArray.java
@@ -1,9 +1,18 @@
 package de.hs_ma.tpews14.ib9.Interface;
 
-public interface AssociativeArray <T> {
-//du bist ein tester!
-	//adsasdad
-	//asdadasd
-	//test obs geht
-	//jojojo
+public interface AssociativeArray <K,V> {
+
+	public void clear();
+	public boolean containsValue(V v);
+	public boolean containsKey(K k);
+	public K get(V v);
+	public boolean isEmpty();
+	public void put(K k, V v);
+	public void putAll();//noch kein Plan
+	public void remove (K v);
+	public int size();
+	public void update();
+	public void forEach(BiConsumer <K,V> b);
+	public void extractAll();//noch kein Plan
+	public void map(BiConsumer<K, V> b);
 }
